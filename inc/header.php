8b1a8b1a5b4def0efdc6e965ac75dc1746db0924
priegelsdev <priegelsdev@gmail.com>
2023-04-10 09:38:48 +0200

    initialize project and install bootstrap

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<!-- <?php include 'config/database.php'; ?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Shop</title>
+
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <a href="#">PHP Shop</a>
+      <ul>
+        <li><a class="text-muted">Home</a></li>
+        <li><a>Shop</a></li>
+        <li><a>Cart</a></li>
+      </ul>
+    </nav>
+  </header>
